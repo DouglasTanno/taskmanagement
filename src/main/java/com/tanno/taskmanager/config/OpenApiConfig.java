e308bf044bfd980e8c63695fdf06b8b08112c13c
Douglas Tanno <douglas.tanno@gmailcom>
2026-08-04 19:47:51 -0300

    feat: configuração inicial Swagger

---
+++ src/main/java/com/tanno/taskmanager/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.tanno.taskmanager.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI taskManagerOpenAPI() {
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Task Manager API")
+                        .description("REST API para gerenciamento de projetos e tarefas.")
+                        .version("1.0.0"));
+    }
+}
